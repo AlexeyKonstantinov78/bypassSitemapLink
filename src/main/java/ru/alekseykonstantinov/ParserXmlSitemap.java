21662b35a14a7b5c67ba11fe01927a97edacb60e
AlexeyKonstantinov78 <alex_kav@mail.ru>
2025-05-05 14:10:10 +0300

    add logging apdate sendAsync

--- src/main/java/ru/alekseykonstantinov/ParserXmlSitemap.java
+++ src/main/java/ru/alekseykonstantinov/ParserXmlSitemap.java
@@ -16,14 +16,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public class ParserXmlSitemap {
-    
+
     private static List<String> listXml = new ArrayList<>();
     private static List<String> listUrl = new ArrayList<>();
 
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException, ExecutionException {
         URI uri = new URI("https://nasosyvodoly.ru/sitemap_index.xml");
 
         HttpResponse<InputStream> httpResponse = sendHttpClient(uri);
@@ -60,11 +61,11 @@ public class ParserXmlSitemap {
         }
     }
 
-    public static HttpResponse<InputStream> sendHttpClient(URI uri) throws IOException, InterruptedException {
+    public static HttpResponse<InputStream> sendHttpClient(URI uri) throws InterruptedException, ExecutionException {
 
         HttpClient hl2 = HttpClient.newHttpClient();
         HttpRequest httpRequest = HttpRequest.newBuilder().GET().uri(uri).build();
-        HttpResponse<InputStream> httpResponse = hl2.send(httpRequest, HttpResponse.BodyHandlers.ofInputStream());
+        HttpResponse<InputStream> httpResponse = hl2.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofInputStream()).get();
         log.info(getDateFormat() + " " + uri + " status code: " + httpResponse.statusCode());
         //System.out.println(getDateFormat() + " " + uri + " status code: " + httpResponse.statusCode());
         return httpResponse;
@@ -83,14 +84,12 @@ public class ParserXmlSitemap {
 
                     if (type.equals("sitemap") && reader.getLocalName().equals("loc")) {
                         String loc = reader.getElementText();
-                        //System.out.println("loc: " + loc);
                         listXml.add(loc);
                     }
 
                     if (type.equals("url") && reader.getLocalName().equals("loc")) {
                         if (reader.getLocalName().equals("loc")) {
                             String loc = reader.getElementText();
-                            //System.out.println("loc: " + loc);
                             listUrl.add(loc);
                         }
                     }
@@ -103,7 +102,7 @@ public class ParserXmlSitemap {
     }
 
     public static void printInputStream(InputStream input) throws IOException {
-        System.out.println("\nTeлo: ");
+        log.info("\nTeлo: ");
         int с;
         // Прочитать и отобразить все тело .
         while ((с = input.read()) != -1) {
